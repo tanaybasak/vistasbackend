0fda2d4710b1a81ae2a3b385cdee65fb9225ee90
Tanoy Basak <TBASAK@beckman.com>
2024-12-12 21:30:42 +0530

    changes git

--- db/connection.js
+++ db/connection.js
@@ -2,17 +2,14 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectToDatabase = async() => {
-    mongoose.set('debug', true);
+        mongoose.set('debug', true);
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
-};
+        try {
+            await mongoose.connect(process.env.MONGODB_URI; console.log('Connected to MongoDB');
+            }
+            catch (error) {
+                console.error('Error connecting to MongoDB:', error);
+            }
+        };
 
-connectToDatabase();
+        connectToDatabase();
